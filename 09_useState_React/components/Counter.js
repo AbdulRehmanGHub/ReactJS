6dda53076db01e69924f51b0e940c899271c6fe1
abdulrehmanghub <pakkhan9009@gmail.com>
2024-11-04 14:48:43 +0500

    Counter App

--- 09_useState_React/components/Counter.js
+++ 09_useState_React/components/Counter.js
@@ -1,1 +1,16 @@
-// // coding removed :(
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div style={{ textAlign: "center", margin: "2rem 0" }}>
+      <h2>Count: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>+ Count</button>
+      <button onClick={() => setCount(count - 1)}>- Count</button>
+    </div>
+  );
+};
+
+export default Counter;
+
+// // I've used useState Hook, and this is a basic example of useState.
